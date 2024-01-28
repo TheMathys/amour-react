511b4f7af4127444a5a08d439be48f7268ffb599
TheMathys <mathys.besnard@etudiant.univ-lr.fr>
2024-01-28 15:04:45 +0100

    ajout du routeur

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import './App.css';
 import Navbar from "./components/Navbar";
 import Game from "./components/Game";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes
+} from "react-router-dom";
+
+function NoRoute() {
+    return (
+        <div>
+            <h1>404 pas trouvé</h1>
+        </div>
+    )
+}
+
+function Home()
+{
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
 
 function App() {
-  return (
-    <div style={{position: "absolute", background: "black", width: "100%", height: "100%"}}>
-         <Navbar />
-         <Game />
-      </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/game" element={<Game />} />
+                <Route path="*" element={<NoRoute />} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App;
